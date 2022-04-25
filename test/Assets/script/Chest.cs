ce95432ec3f06695846aed38ebf94fca88c30507
Jonson324 <80275720+Jonson324@users.noreply.github.com>
2022-04-25 03:48:29 +0300

    Добавил очертания комнаты обучения и пару мелки исправлений

--- test/Assets/script/Chest.cs
+++ test/Assets/script/Chest.cs
@@ -7,16 +7,32 @@ public class Chest : MonoBehaviour
 {
     [SerializeField] private List<Items> items;
 
+    public GameObject hint;
 
-    public void Useble()
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            hint.SetActive(true);
+        }
+    }
+    void OnTriggerStay(Collider other)
     {
         if (Input.GetKey(KeyCode.E))
         {
+            hint.SetActive(false);
+
             Instantiate(items[Random.Range(0, items.Count)], transform.position, Quaternion.identity); //Берётся рандомный предмет через рандомное взятое число от 0 до количества предметов
+
+
         }
     }
 
-    
+    void OnTriggerExit(Collider other)
+    {
+        hint.SetActive(false);
+    }
 
 
 
