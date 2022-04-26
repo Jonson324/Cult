d65375c5ecae564078e4406c3ca1d9546e9e4b05
Jonson324 <80275720+Jonson324@users.noreply.github.com>
2022-04-26 20:41:00 +0300

    Работа с доп элементами

---
+++ test/Assets/script/HpObject.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HpObject : MonoBehaviour
+{
+    public int HealthObj = 100;
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "fireboll")
+        {
+            HealthObj -= 50;
+            Debug.Log(HealthObj);
+            if (HealthObj <= 0)
+            {
+                Destroy(gameObject);
+            }
+
+        }
+
+    }
+}
