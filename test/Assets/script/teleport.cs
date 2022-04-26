a9cb8a6a0802bcf46f0616955915322269fc06eb
IvanKrasawin <ivancrasawin@yandex.ru>
2022-04-26 16:42:09 +0300

    prefab

--- test/Assets/script/teleport.cs
+++ test/Assets/script/teleport.cs
@@ -4,5 +4,15 @@ using UnityEngine;
 
 public class teleport : MonoBehaviour
 {
-
+    public GameObject teleportPoint;
+    
+ 
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            Debug.Log("go");
+            other.gameObject.transform.position = teleportPoint.gameObject.transform.position;
+        }
+    }
 }
